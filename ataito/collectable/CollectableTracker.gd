91f8f905d39ef060d65492034eed174c51886dc2
Samuel Sarette <samuelsarette@linux.com>
2022-01-29 18:28:30 -0500

    track amount and total

--- ataito/collectable/CollectableTracker.gd
+++ ataito/collectable/CollectableTracker.gd
@@ -1,24 +1,29 @@
 extends Spatial
 class_name CollectableTracker
 
-var collected_total := 0 setget , _get_total
+var collectable_total := 0
+var collected_amount := 0 setget , _get_collected
 var collected_light := 0
 var collected_dark := 0
 
+signal total_updated(amount, total)
+
 func _ready():
 	var collectables = get_tree().get_nodes_in_group("collectable")
+	collectable_total = collectables.size()
 	for c in collectables:
 		c.connect("collected", self, "on_Collectable_collected")
 
-
 func on_Collectable_collected(type, _node):
 	if type == Collectable.Alignment.Light:
-		print("light collected")
+		#print("light collected")
 		collected_light += 1
 	else:
-		print("dark collected")
+		#print("dark collected")
 		collected_dark += 1
-	print("total of {num} collected".format({'num': _get_total()}))
+	var collected := _get_collected()
+	print("total of {num} collected of {total}".format({'num': collected, 'total': collectable_total}))
+	emit_signal("total_updated", collected, collectable_total)
 
-func _get_total() -> int:
+func _get_collected() -> int:
 	return collected_light + collected_dark
