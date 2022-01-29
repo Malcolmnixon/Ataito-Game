29a819483529322121f6e32e81c0ac775bd0d2bb
Malcolm Nixon <malcolm.nixon@gmail.com>
2022-01-28 20:21:31 -0500

    Fixed hand finger position scripts (until the OpenXR fix for Oculus arrives) Added Ape hands and texture.

--- addons/godot-xr-tools/assets/LeftHand.gd
+++ addons/godot-xr-tools/assets/LeftHand.gd
@@ -7,6 +7,10 @@ func _process(delta):
 		var grip = controller.get_joystick_axis(JOY_VR_ANALOG_GRIP) * 2.5
 		var trigger = controller.get_joystick_axis(JOY_VR_ANALOG_TRIGGER) * 2.5
 		
+		# Hack for finger locations until XR fix
+		grip = clamp(grip, 0.0, 2.5)
+		trigger = clamp(trigger, 0.0, 2.5)
+		
 		# print("Grip: " + str(grip) + " Trigger: " + str(trigger))
 		
 		$AnimationTree.set("parameters/SetGrip/seek_position", grip)
