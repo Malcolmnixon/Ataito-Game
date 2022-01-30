48424004a2a6b9363cd3fc2b9d14a6dc091d3460
Samuel Sarette <samuelsarette@linux.com>
2022-01-30 10:42:56 -0500

    More sound work

--- ataito/water_spout/WaterSpout.gd
+++ ataito/water_spout/WaterSpout.gd
@@ -6,13 +6,13 @@ func _ready():
 	#change to blocked-up sound
 	_sound.max_distance = 50
 	_sound.pitch_scale = 0.5
-	_sound.unit_db = 15
+	_sound.unit_db = 18
 
 func _on_GrabDestroyBoulder_destroyed():
 	$Spout.emitting = true
 	$WindArea/CollisionShape.disabled = false
 
 	#change to spraying sound
-	_sound.max_distance = 100
+	_sound.max_distance = 70
 	_sound.pitch_scale = 1
 	_sound.unit_db = 50
