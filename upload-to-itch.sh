fb5afcc4e94bafba12113dc86fd0cea32a9378fb
Samuel Sarette <samuelsarette@linux.com>
2022-01-30 13:07:17 -0500

    more release work

--- upload-to-itch.sh
+++ upload-to-itch.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 CURRENT_DIR=$(dirname "$(readlink -f "$0")")
 APK="$CURRENT_DIR/build/Ataito.apk"
+WINDOWS="$CURRENT_DIR/build/windws/"
 
 echo Uploading to Itch...
 butler -v push "$APK" "malcolmnixon/ataito-the-spirit-swapper:android"
+butler -v push "$WINDOWS" "malcolmnixon/ataito-the-spirit-swapper:windows"
 
 echo Done.
