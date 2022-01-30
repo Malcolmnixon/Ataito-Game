90fbf905bcc73088492d7e33238b98c3f7ccd794
Samuel Sarette <samuelsarette@linux.com>
2022-01-29 22:00:58 -0500

    Make level end show your orbs

---
+++ ataito/collectable/SpiritOrb.gd
@@ -0,0 +1,33 @@
+tool
+extends Particles
+class_name SpiritOrb
+
+var mat_default : Material = preload("res://ataito/collectable/material/Default.material")
+var mat_light : Material = preload("res://ataito/collectable/material/Light.material")
+var mat_dark : Material = preload("res://ataito/collectable/material/Dark.material")
+
+var pass_default : Mesh = preload("res://ataito/collectable/pass/pass_default.tres")
+var pass_dark : Mesh = preload("res://ataito/collectable/pass/pass_dark.tres")
+
+enum Alignment {
+	Light = 1,
+	Dark = 2
+}
+
+export (Alignment) var type := Alignment.Light setget update_type
+
+func _ready():
+	update_type(type)
+
+func update_type(value):
+	type = value
+	match type:
+		Alignment.Light:
+			draw_pass_1 = pass_default
+			process_material = mat_light
+		Alignment.Dark:
+			draw_pass_1 = pass_dark
+			process_material = mat_dark
+		_:
+			draw_pass_1 = pass_default
+			process_material = mat_default
