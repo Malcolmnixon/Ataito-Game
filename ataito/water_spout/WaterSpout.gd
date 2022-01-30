b6715c1b930cfc964b7292d3e590f41adfac144b
Samuel Sarette <samuelsarette@linux.com>
2022-01-30 10:27:52 -0500

    add water spout sound

--- ataito/water_spout/WaterSpout.gd
+++ ataito/water_spout/WaterSpout.gd
@@ -1,6 +1,18 @@
 extends Spatial
 
+onready var _sound := $Sound
+
+func _ready():
+	#change to blocked-up sound
+	_sound.max_distance = 50
+	_sound.pitch_scale = 0.5
+	_sound.unit_db = 15
+
 func _on_GrabDestroyBoulder_destroyed():
 	$Spout.emitting = true
 	$WindArea/CollisionShape.disabled = false
-	pass # Replace with function body.
+
+	#change to spraying sound
+	_sound.max_distance = 100
+	_sound.pitch_scale = 1
+	_sound.unit_db = 50
