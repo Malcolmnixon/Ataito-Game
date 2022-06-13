630679f6009f52a0b109c4e687f70be19be86ca2
Malcolm Nixon <Malcolm.nixon@gmail.com>
2022-06-12 22:39:53 -0400

    Updated godot-openxr to 1.3.0 pre-release 2340c01 (June 5th 2022) Updated zylann.hterrain to 1.6

--- addons/godot-openxr/scenes/first_person_controller_vr.gd
+++ addons/godot-openxr/scenes/first_person_controller_vr.gd
@@ -1,5 +1,6 @@
-extends ARVROrigin
+tool
 class_name FirstPersonControllerVR
+extends ARVROrigin
 
 signal initialised
 signal failed_initialisation
@@ -11,34 +12,68 @@ signal focused_state
 signal visible_state
 signal pose_recentered
 
-export var auto_initialise = true
-export var start_passthrough = false
-export (NodePath) var viewport = null
+export var auto_initialise = true setget set_auto_initialise
+export var enable_passthrough = false setget set_enable_passthrough
+export (NodePath) var viewport setget set_viewport
+export var near_z = 0.1
+export var far_z = 1000.0
 
 var interface : ARVRInterface
+var enabled_extensions : Array
+
+func set_auto_initialise(p_new_value):
+	auto_initialise = p_new_value
+	update_configuration_warning()
+
+func set_enable_passthrough(p_new_value):
+	enable_passthrough = p_new_value
+	update_configuration_warning()
+
+	# Only actually start our passthrough if our interface has been instanced
+	# if not this will be delayed until initialise is successfully called.
+	if interface:
+		if enable_passthrough:
+			# unset enable_passthrough if we can't start it.
+			enable_passthrough = _start_passthrough()
+		else:
+			_stop_passthrough()
+
+func set_viewport(p_new_value):
+	viewport = p_new_value
+	update_configuration_warning()
 
 func get_interface() -> ARVRInterface:
 	return interface
 
 func _ready():
-	if auto_initialise:
+	$ARVRCamera.near = near_z
+	$ARVRCamera.far = far_z
+
+	if auto_initialise && !Engine.editor_hint:
 		initialise()
 
 func initialise() -> bool:
+	if Engine.editor_hint:
+		print("Can't initialise while in the editor")
+		return false
+
 	if interface:
 		# we are already initialised
 		return true
 
 	interface = ARVRServer.find_interface("OpenXR")
-	var initialized := true if interface.interface_is_initialized else interface.initialize()
-	if interface and initialized:
+	if interface and interface.initialize():
 		print("OpenXR Interface initialized")
 
 		# Find the viewport we're using to render our XR output
 		var vp : Viewport = _get_xr_viewport()
 
+		# Obtain enabled extensions
+		enabled_extensions = $Configuration.get_enabled_extensions()
+
 		# Start passthrough?
-		_start_passthrough()
+		if enable_passthrough and is_passthrough_supported():
+			enable_passthrough = _start_passthrough()
 
 		# Connect to our plugin signals
 		_connect_plugin_signals()
@@ -46,8 +81,7 @@ func initialise() -> bool:
 		# Change our viewport so it is tied to our ARVR interface and renders to our HMD
 		vp.arvr = true
 
-		# Our interface will tell us whether we should keep our render buffer in linear color space
-		vp.keep_3d_linear = $Configuration.keep_3d_linear()
+		# We can't set keep linear yet because we won't know the correct value until after our session has begun.
 
 		# increase our physics engine update speed
 		var refresh_rate = $Configuration.get_refresh_rate()
@@ -66,6 +100,8 @@ func initialise() -> bool:
 		emit_signal("initialised")
 		return true
 	else:
+		interface = null
+
 		emit_signal("failed_initialisation")
 		return false
 
@@ -76,13 +112,22 @@ func _get_xr_viewport() -> Viewport:
 	else:
 		return get_viewport()
 
-func _start_passthrough():
-	if start_passthrough:
-		# make sure our viewports background is transparent
-		_get_xr_viewport().transparent_bg = true
+func is_passthrough_supported() -> bool:
+	var supported = enabled_extensions.find("XR_FB_passthrough") >= 0
+	return supported
 
-		# enable our passthrough
-		$Configuration.start_passthrough()
+func _start_passthrough() -> bool:
+	# make sure our viewports background is transparent
+	_get_xr_viewport().transparent_bg = true
+
+	# enable our passthrough
+	return $Configuration.start_passthrough()
+
+func _stop_passthrough():
+	# make sure our viewports background is not transparent
+	_get_xr_viewport().transparent_bg = false
+
+	$Configuration.stop_passthrough()
 
 func _connect_plugin_signals():
 	ARVRServer.connect("openxr_session_begun", self, "_on_openxr_session_begun")
@@ -93,6 +138,12 @@ func _connect_plugin_signals():
 
 func _on_openxr_session_begun():
 	print("OpenXR session begun")
+
+	var vp : Viewport = _get_xr_viewport()
+	if vp:
+		# Our interface will tell us whether we should keep our render buffer in linear color space
+		vp.keep_3d_linear = $Configuration.keep_3d_linear()
+
 	emit_signal("session_begun")
 
 func _on_openxr_session_ending():
@@ -110,3 +161,23 @@ func _on_openxr_visible_state():
 func _on_openxr_pose_recentered():
 	print("OpenXR pose recentered")
 	emit_signal("pose_recentered")
+
+func _get_configuration_warning():
+	var version = Engine.get_version_info()
+
+	if viewport:
+		var vp = get_node(viewport)
+		if !vp:
+			return "Can't access assigned viewport"
+		if !(vp is Viewport):
+			return "Selected viewport is not a viewport"
+		if vp.render_target_update_mode != Viewport.UPDATE_ALWAYS:
+			return "Viewport update mode is not set to always, you may not get proper output"
+
+	if enable_passthrough and version['major'] == 3 and version['minor'] < 4:
+		return "Godot %s is too old for the passthrough version. Please upgrade to Godot 3.4 or later." % version['string']
+
+	if !auto_initialise:
+		return "You must call initialise() manually for VR to start"
+
+	return ""
